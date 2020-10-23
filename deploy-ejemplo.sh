487038a51ce8e9c158a40c82c5c248520439232a
vacax <ccamachog@avathartech.com>
2020-10-23 02:57:12 -0400

    Añadiendo el comando para descargar el template.

--- deploy-ejemplo.sh
+++ deploy-ejemplo.sh
@@ -2,5 +2,6 @@
 echo "Iniciando Proyecto Demostración OpenFaaS con java"
 echo "Subiendo la base de datos"
 docker run --name mysql-openfaas --rm -it -d -e MYSQL_ROOT_PASSWORD=12345678 -e MYSQL_DATABASE=openfaas -p 3306:3306 mysql:5.7.32
+faas-cli template pull
 faas-cli deploy -f demo-java-openfaas.yml
 echo "Demo Disponible"
