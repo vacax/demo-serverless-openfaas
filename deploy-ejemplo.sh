77de8636516df97b4203e6df9c91b2eda3d18707
vacax <ccamachog@avathartech.com>
2020-10-23 02:45:27 -0400

    Incluyendo la opción de daemon en la base de datos.

--- deploy-ejemplo.sh
+++ deploy-ejemplo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "Iniciando Proyecto Demostración OpenFaaS con java"
 echo "Subiendo la base de datos"
-docker run --name mysql-openfaas --rm -it -e MYSQL_ROOT_PASSWORD=12345678 -e MYSQL_DATABASE=openfaas -p 3306:3306 mysql:5.7.32
+docker run --name mysql-openfaas --rm -it -d -e MYSQL_ROOT_PASSWORD=12345678 -e MYSQL_DATABASE=openfaas -p 3306:3306 mysql:5.7.32
 faas-cli deploy -f demo-java-openfaas.yml
 echo "Demo Disponible"
